38e4d6dfef53b20899b2751c33c03b1fc893aacc
kichu12348 <rmahadevan574@gmail.com>
2024-02-24 17:05:53 +0530

    minor fix2

--- controllers/user.js
+++ controllers/user.js
@@ -121,8 +121,7 @@ async function deleteContact(req,res){
     const roomID = user.contacts.find(contact=>contact.contact===id).roomID;
     await User.findByIdAndUpdate(user._id.toString(),{$pull:{contacts:{contact:id}}});
     await User.findByIdAndUpdate(id,{$pull:{contacts:{contact:user._id.toString()}}});
-    //await Message.deleteMany({roomID});
-    console.log(roomID)
+    await Message.deleteMany({roomID});
     return res.json({response:true, message:'contact deleted successfully'});
 }
   
