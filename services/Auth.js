8adad192f4f77e2892b364b0d5d2c736fd4a578e
Mahadevan Reji <rmahadevan574@gmail.com>
2024-06-11 00:52:22 +0530

    jwt fix

--- services/Auth.js
+++ services/Auth.js
@@ -1,32 +1,43 @@
-const jwt = require('jsonwebtoken');
-const secret = process.env.SECRET_CODE
-const User = require('../models/user');
+const jwt = require("jsonwebtoken");
+const secret = process.env.SECRET_CODE;
+const User = require("../models/user");
 
-async function createToken(id){
-    const token = await jwt.sign(id, secret);
-    return token;
+async function createToken(id) {
+  const token = await jwt.sign(id, secret);
+  return token;
 }
 
-async function verifyToken(token){
+async function verifyToken(token) {
+  try {
     const id = await jwt.verify(token, secret);
     return await User.findById(id);
+  } catch (err) {
+    return null;
+  }
 }
 
-async function auth(req,res,next){
-    const token = req.body.token;
-    if(!token) return res.status(401).json({response:false, message:'Unauthorized'});
-    const user = await verifyToken(token);
-    if(!user) return res.status(401).json({response:false, message:'Unauthorized'});
-    req.user = user;
-    next();
+async function auth(req, res, next) {
+  const token = req.body.token;
+  if (!token)
+    return res.status(401).json({ response: false, message: "Unauthorized" });
+  const user = await verifyToken(token);
+  if (!user)
+    return res.status(401).json({ response: false, message: "Unauthorized" });
+  req.user = user;
+  next();
 }
 
-async function checkAuth(req,res){
-    const token = req.body.token;
-    if(!token) return res.json({response:false});
-    const user = await verifyToken(token);
-    if(!user) return res.json({response:false});
-    res.json({response:true, name:user.user, email:user.email,pic:user.profilePic});
+async function checkAuth(req, res) {
+  const token = req.body.token;
+  if (!token) return res.json({ response: false });
+  const user = await verifyToken(token);
+  if (!user) return res.json({ response: false });
+  res.json({
+    response: true,
+    name: user.user,
+    email: user.email,
+    pic: user.profilePic,
+  });
 }
 
-module.exports = {createToken, verifyToken, auth, checkAuth};
+module.exports = { createToken, verifyToken, auth, checkAuth };
